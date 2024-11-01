cdc547aeac55d335e5f9ff0b5a4ef08a7835952a
dabrze <dariusz.brzezinski@cs.put.poznan.pl>
2024-11-01 16:15:05 +0100

    Improved computeMapModelDifference.sh

--- computeMapModelDifference.sh
+++ computeMapModelDifference.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Author: John Heumann
+
 if [ $# -ne 3 ]
 then
   echo "Usage: $0 <dir> <map> <model>"
+  echo "  Construct a cryoEM map - model difference map using Phenix"
+  echo "    <dir>  : the directory containing <map> and <model>"
+  echo "    <map>  : a cryoEM map of the <model> plus any ligands"
+  echo "    <model>: a ligand-free .pdb or .cif model of the <map>"
   exit 1
 fi
 
@@ -14,11 +20,11 @@ cd "$DIR" >/dev/null
 
 if [ "$TYPE" == "pdb" ]
 then
-  RES=$(grep -i 'RESOLUTION.*ANGSTROMS' *.pdb | head -1 | \
+  RES=$(grep -i 'RESOLUTION.*ANGSTROMS' "$MODEL" | head -1 | \
     sed -e's/  */ /g' | cut -f 4 -d ' ')
 elif [ "$TYPE" == "cif" ]
 then
-  RES=$(grep -i reconstruction.resolution *stripped.cif | grep -v method | \
+  RES=$(grep -i reconstruction.resolution "$MODEL" | grep -v method | \
     sed -e's/  */ /g' | cut -f 2 -d ' ')
 else
   echo "ERROR: unrecognized model type!"
@@ -27,5 +33,7 @@ fi
 
 echo "phenix.real_space_diff_map $MODEL $MAP resolution=$RES"
 phenix.real_space_diff_map "$MODEL" "$MAP" "resolution=$RES"
+rm -rf map_model_difference_2.ccp4
+echo "Difference map is in $(pwd)/map_model_difference_1.ccp4"
 
 exit 0
